34180b9fd73b7fd6bbcdcb86cb14a0cc933c6be9
KDpractuce142 <167613706+KDpractuce142@users.noreply.github.com>
2026-08-01 13:54:45 +0300

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -4,30 +4,16 @@ import arc.*;
 import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import mindustry.world.blocks.logic.MessageBlock.*;
 
-public class ExampleJavaMod extends Mod{
-
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
+public class ExampleMod extends Mod {
+    @Override
+    public void init(){
+        Events.on(ConfigEvent.class, event -> {
+            if(event.tile instanceof MessageBuild message){
+                Core.files.external("scrap_log.txt").appendString(message.message + "\n");
+                Log.info("Записано в log: " + message.message);
+            }
         });
     }
-
-    @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
-    }
-
 }
